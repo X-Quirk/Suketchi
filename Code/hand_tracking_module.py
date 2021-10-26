da88b5e1c2f1988f76c00ead44e4b21665030c03
X Quirk <sangeethrps03@gmail.com>
2021-10-26 11:00:42 +0530

    Formatted code structure

--- Code/hand_tracking_module.py
+++ Code/hand_tracking_module.py
@@ -1,35 +1,40 @@
-import cv2 
+import cv2
 import mediapipe as mp
 import time
 
+
 class handDetector():
     def __init__(self,
-                 mode = False,
-                 max_hands = 2,
-                 detection_confidence = 0.5,
-                 track_confidence =  0.5):
+                 mode=False,
+                 max_hands=2,
+                 detection_confidence=0.5,
+                 track_confidence=0.5):
         self.mode = mode
         self.max_hands = max_hands
         self.detection_confidence = detection_confidence
         self.track_confidence = track_confidence
 
         self.mp_hands = mp.solutions.hands
-        self.hands = self.mp_hands.Hands(self.mode,self.max_hands,
-                                         self.detection_confidence,self.track_confidence)
-        self.mp_draw = mp.solutions.drawing_utils # Drawing the line between two landmarks
-        self.tip_ids = [4, 8, 12, 16, 20] # Landmark id of the tip of the fingers
+        self.hands = self.mp_hands.Hands(self.mode, self.max_hands,
+                                         self.detection_confidence, self.track_confidence)
+        # Drawing the line between two landmarks
+        self.mp_draw = mp.solutions.drawing_utils
+        # Landmark id of the tip of the fingers
+        self.tip_ids = [4, 8, 12, 16, 20]
 
-    def findHands(self, img, draw = True):
-        
-        img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Because mediapipe functions only take RGB Images
-   
+    def findHands(self, img, draw=True):
+
+        # Because mediapipe functions only take RGB Images
+        img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
         self.results = self.hands.process(img_RGB)
 
         if self.results.multi_hand_landmarks:
-            for hand_landmarks in self.results.multi_hand_landmarks:   
-                if draw:   
+            for hand_landmarks in self.results.multi_hand_landmarks:
+                if draw:
                     # Detecting the landmarks and Drawing them
-                    self.mp_draw.draw_landmarks(img,hand_landmarks,self.mp_hands.HAND_CONNECTIONS)         
+                    self.mp_draw.draw_landmarks(
+                        img, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
 
         return img
 
@@ -43,42 +48,41 @@ class handDetector():
             for id, landmark in enumerate(hand.landmark):
                 #  print(id,landmark)
                 height, width, channels = img.shape
-                center_x,  center_y = int(landmark.x * width), int(landmark.y * height)
-                self.landmark_list[id] = [center_x,center_y]
+                center_x,  center_y = int(
+                    landmark.x * width), int(landmark.y * height)
+                self.landmark_list[id] = [center_x, center_y]
                 if draw:
-                    cv2.circle(img, (center_x,center_y),7,(0,255,0),cv2.FILLED) 
+                    cv2.circle(img, (center_x, center_y),
+                               7, (0, 255, 0), cv2.FILLED)
         return self.landmark_list
 
     def fingersUp(self):
         fingers = []
 
         # Detection for Thumb
-        if self.landmark_list[self.tip_ids[0]][0] < self.landmark_list[self.tip_ids[0]-1][0]:  
-                fingers.append(1) 
+        if self.landmark_list[self.tip_ids[0]][0] < self.landmark_list[self.tip_ids[0]-1][0]:
+            fingers.append(1)
         else:
             fingers.append(0)
-  
+
         # Detection for other fingers
-        for id in range(1,5):
-            if self.landmark_list[self.tip_ids[id]][1] < self.landmark_list[self.tip_ids[id]-2][1]: # Because top most value in opencv is lower 
+        for id in range(1, 5):
+            # Because top most value in opencv is lower
+            if self.landmark_list[self.tip_ids[id]][1] < self.landmark_list[self.tip_ids[id]-2][1]:
                 fingers.append(1)
             else:
                 fingers.append(0)
         return fingers
 
-                
-
-
 
 def main():
-    # For frame rates 
+    # For frame rates
     prev_time = 0
     current_time = 0
 
     cap = cv2.VideoCapture(0)
 
     detector = handDetector()
-
 
     while True:
         success, img = cap.read()
@@ -93,12 +97,14 @@ def main():
         fps = 1/(current_time-prev_time)
         prev_time = current_time
 
-        cv2.putText(img, str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,2,(255,0,0),2) # Viewing the fps
+        cv2.putText(img, str(int(fps)), (10, 70),
+                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)  # Viewing the fps
 
         cv2.imshow('Image', img)
-    
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
 
 if __name__ == '__main__':
     main()
